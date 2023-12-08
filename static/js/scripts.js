ef4c44daaee5e6583081e95e905a9ebeee562d4e
JobRodriguez1 <llesau.2003@gmail.com>
2023-12-07 22:56:15 -0600

    Filtros cursos

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -115,16 +115,6 @@ window.addEventListener('resize', () => {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Verifica si la URL actual contiene "cursos.html"
-
-    //restablecer
-    $('.Restablecer').click(function (event) {
-        event.preventDefault(); // Evita que el enlace redireccione directamente
-        location.href = "Cursos"
-
-    });
-
-    // Tu código AJAX aquí
     $(document).ready(function () {
         // Función para realizar la búsqueda
         function realizarBusqueda() {
@@ -150,95 +140,50 @@ document.addEventListener('DOMContentLoaded', function () {
         });
 
         // Captura el cambio en el filtro de categorías
-        // Obtiene la categoría de la URL
-        var categoriaEnUrl = obtenerCategoriaDeUrl();
-
-        // Si hay una categoría en la URL, establece el valor del select y activa el filtrado
-        if (categoriaEnUrl) {
-            $('#filtro-categorias').val(categoriaEnUrl);
-
-            // Aquí puedes agregar la lógica de filtrado directamente o activar el evento 'change'
-            // dependiendo de cómo estés manejando el filtro en tu código actual
-            $('#filtro-categorias').trigger('change');
-        }
-
-        // Maneja clics en los enlaces de categoría
-        $('.filtro-link').click(function (event) {
-            event.preventDefault(); // Evita que el enlace redireccione directamente
-
-            var categoria = $(this).data('categoria');
-            window.location.href = $(this).attr('href'); // Redirige a la URL del enlace
-
-            // Aquí puedes ajustar la lógica según tus necesidades, como activar el filtrado directamente
-            // o realizar otras acciones antes de la redirección
-        });
-        });
-
-        function obtenerCategoriaDeUrl() {
-        var urlParams = new URLSearchParams(window.location.search);
-        return urlParams.get('categoria');
-        }
-
-        // Resto de tu código de filtrado AJAX
         $('#filtro-categorias').change(function () {
-        var selectedCategoria = $(this).val();
+            var selectedCategoria = $(this).val();
 
-        // Oculta todos los videos
-        $('.video-item').hide();
+            // Oculta todos los videos
+            $('.video-item').hide();
 
-        // Muestra los videos que tienen la categoría seleccionada o todos si es "Todos"
-        if (selectedCategoria === 'Todos') {
-            $('.video-item').show();
-        } else {
-            // Itera sobre los elementos y muestra aquellos que tienen la categoría seleccionada
-            $('.video-item').each(function () {
-                var categoriasVideo = $(this).data('categorias');
+            // Muestra los videos que tienen la categoría seleccionada o todos si es "Todos"
+            if (selectedCategoria === 'Todos') {
+                $('.video-item').show();
+            } else {
+                $('.video-item').each(function () {
+                    var categoriasVideo = $(this).data('categorias');
 
-                if (categoriasVideo.includes(selectedCategoria)) {
-                    $(this).show();
-                }
-            });
-        }
-
+                    if (categoriasVideo.includes(selectedCategoria)) {
+                        $(this).show();
+                    }
+                });
+            }
+        });
 
         // Captura el cambio en el filtro de orden
         $('#ordenar-por').change(function () {
             var opcionSeleccionada = $(this).val();
 
-            if (opcionSeleccionada === 'AgregadosRecientemente') {
-                // Ordena los videos por fecha de agregado
-                ordenarPorFecha();
-            } else if (opcionSeleccionada === 'Alfabeticamente') {
-                // Ordena los videos alfabéticamente por título
-                ordenarAlfabeticamente();
-            }
+            // Realiza una nueva solicitud AJAX al servidor para obtener datos ordenados
+            $.ajax({
+                type: 'GET',
+                url: '/ordenar-videos/',  // Ajusta la ruta a tu endpoint de ordenar
+                data: {
+                    opcion: opcionSeleccionada,
+                    categoria: $('#filtro-categorias').val()  // Agrega la categoría seleccionada
+                },
+                success: function (data) {
+                    // Actualiza la interfaz de usuario con los datos ordenados
+                    $('.video-container').html(data);
+                },
+                error: function (error) {
+                    console.error('Error al obtener datos ordenados:', error);
+                }
+            });
         });
     });
-
-    /*// Función para ordenar los videos por fecha de agregado
-    function ordenarPorFecha() {
-        var videos = $('.video-item').toArray();
-        videos.sort(function (a, b) {
-            var fechaA = new Date($(a).data('fecha'));
-            var fechaB = new Date($(b).data('fecha'));
-            return fechaB - fechaA;
-        });
-        console.log(videos)
-        $('.video-container').empty().append(videos);
-    }
-
-    // Función para ordenar los videos alfabéticamente por título
-    function ordenarAlfabeticamente() {
-        var videos = $('.video-item').toArray();
-        videos.sort(function (a, b) {
-            var tituloA = $(a).find('.video-title').text().toLowerCase();
-            var tituloB = $(b).find('.video-title').text().toLowerCase();
-            return tituloA.localeCompare(tituloB);
-        });
-
-        $('.video-container').empty().append(videos);
-    }*/
 });
+
 
 
 var mostrarAlert = document.querySelectorAll('.mostrarAlerta');
